316f1c9fb2af5e5edf0cadce57cfaf4b271fbd1c
sam224x <151483482+sam224x@users.noreply.github.com>
2023-12-20 17:11:48 -0500

    Commit

---
+++ script.js
@@ -0,0 +1,71 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Simulating loading by delaying the hiding of the splash screen
+    setTimeout(function() {
+      const splashScreen = document.getElementById("splash");
+      const mainContent = document.getElementById("mainContent");
+  
+      splashScreen.style.display = "none";
+      mainContent.style.display = "block";
+    }, 3000); // Change the delay time (in milliseconds) as needed
+  });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const counterElement = document.getElementById("counter");
+    let count = 0;
+  
+    function updateCounter() {
+      if (count <= 100) {
+        counterElement.textContent = count;
+        count++;
+        setTimeout(updateCounter, 25); // Change the time interval (in milliseconds) as desired
+      }
+    }
+  
+    // Start the counter when the DOM content is loaded
+    updateCounter();
+  });
+
+  document.addEventListener("click",e => {
+    let tar = e.target;
+    if (tar.name == "toggle")
+      tar.removeAttribute("class");
+  });
+
+  document.body.onmousemove = function(e) {
+    document.documentElement.style.setProperty (
+      '--x', (
+        e.clientX+window.scrollX
+      )
+      + 'px'
+    );
+    document.documentElement.style.setProperty (
+      '--y', (
+        e.clientY+window.scrollY
+      ) 
+      + 'px'
+    );
+  }
+
+
+// main.js
+document.body.onmousemove = function(e) {
+  document.documentElement.style.setProperty (
+    '--x', (
+      e.clientX+window.scrollX
+    )
+    + 'px'
+  );
+  document.documentElement.style.setProperty (
+    '--y', (
+      e.clientY+window.scrollY
+    ) 
+    + 'px'
+  );
+}
